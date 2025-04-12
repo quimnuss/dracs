127c51b045c49d6101459470d9579b19281729b3
Quim Nuss Székely <quimnuss@altaveu>
2025-04-12 11:03:04 +0200

    camera movement

--- actors/camera_2d.gd
+++ actors/camera_2d.gd
@@ -7,4 +7,6 @@ const HORIZONTAL_MOVEMENT = 1152
 func _process(delta: float) -> void:
     if Input.is_action_just_pressed('ui_right'):
         self.global_position.x += HORIZONTAL_MOVEMENT
+    elif Input.is_action_just_pressed('ui_left'):
+        self.global_position.x -= HORIZONTAL_MOVEMENT
         
