2125602385d828bc0ccfb99087833f33712bb193
Quim Nuss Székely <quimnuss@altaveu>
2025-04-12 12:04:01 +0200

    more desk scene changes, add order text

--- actors/camera_2d.gd
+++ actors/camera_2d.gd
@@ -2,11 +2,17 @@ extends Camera2D
 
 const HORIZONTAL_MOVEMENT = 1152
 
+func go_right():
+    self.global_position.x += HORIZONTAL_MOVEMENT
+    self.global_position.x = clamp(self.global_position.x, self.limit_left + HORIZONTAL_MOVEMENT/2, self.limit_right - HORIZONTAL_MOVEMENT/2)
 
+func go_left():
+    self.global_position.x -= HORIZONTAL_MOVEMENT
+    self.global_position.x = clamp(self.global_position.x, self.limit_left + HORIZONTAL_MOVEMENT/2, self.limit_right - HORIZONTAL_MOVEMENT/2)
 
 func _process(delta: float) -> void:
     if Input.is_action_just_pressed('ui_right'):
-        self.global_position.x += HORIZONTAL_MOVEMENT
+        go_right()
     elif Input.is_action_just_pressed('ui_left'):
-        self.global_position.x -= HORIZONTAL_MOVEMENT
+        go_left()
         
