c1bdf3418cbb0178933e9e766819cce47fc95ddc
Quim Nuss Székely <quimnuss@altaveu>
2025-04-16 00:07:57 +0200

    add two more orders

--- scripts/orders.gd
+++ scripts/orders.gd
@@ -4,29 +4,63 @@ class_name Orders
 # difficulty higher = harder
 
 const orders_dict : Dictionary = {
-    'senyera' : {
-        'order_long': 'Voldria nou roses fent la senyera!',
-        'order_short': '9 roses alternant groc i vermell',
-        'difficulty': 0,
-        'ram': {
-            'flowers' : [Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED],
-            'paper' : 1,
-            'ribbon' : Color.RED
+    "senyera" : {
+        "order_long": "Voldria nou roses fent la senyera!",
+        "order_short": "9 roses alternant groc i vermell",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED],
+            "paper" : 1,
+            "ribbon" : Color.RED
         },
-        'price': 20
+        "price": 20
     },
-    'senyera_invertida' : {
-        'order_long': 'Voldria nou roses fent la senyera! Però invertida',
-        'order_short': '9 roses alternant groc i vermell',
-        'difficulty': 0,
-        'ram': {
-            'flowers' : [Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW],
-            'paper' : 1,
-            'ribbon' : Color.YELLOW
+    "senyera_invertida" : {
+        "order_long": "Voldria nou roses fent la senyera! Però invertida",
+        "order_short": "9 roses alternant groc i vermell",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
         },
-        'price': 25
+        "price": 25
+    },
+    "guiri" : {
+        "order_long": "Hello! Una ramo de fulores, please. Mucho rojo!",
+        "order_short": "5 roses vermelles",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.RED, Color.RED, Color.RED, Color.RED, Color.RED],
+            "paper" : 1,
+            "ribbon" : Color.RED
+        },
+        "price": 25
+    },
+    "culer" : {
+        "order_long": "Bon dia! Ja saps què vull… heh.",
+        "order_short": "5 roses alternant blau i vermell",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.BLUE, Color.RED, Color.BLUE, Color.RED, Color.BLUE],
+            "paper" : 1,
+            "ribbon" : Color.RED
+        },
+        "price": 25
+    },
+  "catalanet" : {
+        "order_long": "Bona diada! Per mi, clàssic. Rosa i espiga!",
+        "order_short": "1 rosa vermella.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.RED],
+            "paper" : 1,
+            "ribbon" : Color.RED
+        },
+        "price": 3
     },
 }
+
 
 static func ordername_to_order(ordername : String) -> Order:
     if not ordername:
