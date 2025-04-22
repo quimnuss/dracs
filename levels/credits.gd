fa753e36c667e087e47b903a4b569ddbda83e6dc
Quim Nuss Székely <quimnuss@altaveu>
2025-04-22 18:21:02 +0200

    canvaslayer for mouse, intro screen

--- levels/credits.gd
+++ levels/credits.gd
@@ -3,6 +3,10 @@ extends CanvasLayer
 @onready var control: Control = $Control
 
 func _ready() -> void:
+    pass
+
+
+func _on_button_pressed() -> void:
     var tween : Tween = create_tween()
-    tween.tween_property(control, 'modulate:a',0, 2).set_delay(3)
+    tween.tween_property(control, 'modulate:a',0, 2)
     tween.tween_callback(queue_free)
