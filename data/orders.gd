162e1902a854d088f309fd4f9c3daf3829d1d28a
Quim Nuss Székely <quimnuss@altaveu>
2025-04-22 19:36:15 +0200

    small tweaks

--- data/orders.gd
+++ data/orders.gd
@@ -71,6 +71,150 @@ const orders_dict : Dictionary[String, Dictionary] = {
         },
         "price": 1
     },
+    
+  "descolorit" : {
+        "order_long": "Estic com indefinit. Una rosa per fer, descolorida.",
+        "order_short": "1 rosa blanca.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.WHITE],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 1
+    },
+
+  "tresina" : {
+        "order_long": "Voldria una rosa de cada color. Ben ordenades de més a menys!",
+        "order_short": "1 rosa vermella, groga, blava i blanca.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.RED,Color.YELLOW,Color.BLUE,Color.WHITE],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 5
+    },
+    
+    "sang" : {
+        "order_long": "Una rosa vermella com la sang!",
+        "order_short": "1 rosa vermella.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.RED],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 1
+    },
+
+    "trist" : {
+        "order_long": "Trobo que fa un dia ben trist avui. Què em recomanaríeu?",
+        "order_short": "1 rosa blava.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.BLUE],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 1
+    },
+    
+    "parella" : {
+        "order_long": "Una rosa per la meva parella Blanca i després una altra per a mi! Estem ben enamorades!",
+        "order_short": "Una rosa blanca i una de vermella.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.WHITE, Color.RED],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 1
+    },
+    
+    "trio" : {
+        "order_long": "Una rosa per les meves parelles i una altra per a mi! Estem ben enamorades!",
+        "order_short": "Tres roses vermelles.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.RED, Color.RED, Color.RED],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 1
+    },
+    
+    "piroman" : {
+        "order_long": "Jo és que ho cremaria tot. Posa'm dues roses.",
+        "order_short": "Una rosa vermella i una de groga.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.RED, Color.YELLOW],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 2
+    },
+    
+    "pluja" : {
+        "order_long": "Ara pla. Per Sant Jordi sempre plou. Que no se'm descolori la rosa tu.",
+        "order_short": "Una rosa blanca i una de blava.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.WHITE, Color.BLUE],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 2
+    },
+    
+    "perico" : {
+        "order_long": "Blanc i blau! Que sóc perico!",
+        "order_short": "Una rosa blanca i una de blava.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.WHITE, Color.BLUE],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 2
+    },
+    
+    "tripletrist" : {
+        "order_long": "Jo és que em poso trist, molt trist.",
+        "order_short": "Tres roses blaves.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.BLUE, Color.BLUE, Color.BLUE],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 4
+    },
+    
+    "aigua" : {
+        "order_long": "M'encanta la vista del mar, amb el cel a sobre",
+        "order_short": "Una rosa groga i dues de blaves.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.YELLOW, Color.BLUE, Color.BLUE],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 4
+    },
+    
+    "groc" : {
+        "order_long": "Ben groc! Com la pell del meu estimat.",
+        "order_short": "Tres roses grogues.",
+        "difficulty": 0,
+        "ram": {
+            "flowers" : [Color.YELLOW, Color.YELLOW, Color.YELLOW],
+            "paper" : 1,
+            "ribbon" : Color.YELLOW
+        },
+        "price": 4
+    },
 }
 
 
